f7b17742da995c5b42db457a359ead1136430677
ilovlowphy <ilovlowphy@gmail.com>
2020-09-26 05:48:44 +0800

    init Addtodo Ui

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <input type="text"> <input type="submit" value="Submit" class="btn">
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+}
+input[type='submit'] {
+  flex: 2;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
+    <AddTodo />
     <h1>This is Home page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import AddTodo from '@/components/AddTodo.vue'
 
 export default {
   name: 'Home',
   components: {
+    AddTodo
   }
 }
 </script>
